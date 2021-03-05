03ee241d4800c94ad3bfaa5a8a0753f6dc786970
Fraijo17 <abrahamfraijo17@gmail.com>
2021-03-04 21:16:53 -0700

    0

---
+++ routes/api/apiRoutes.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const { Workout } = require("../../models");
+
+router.get("/workouts", (req, res) => {
+  Workout.find({})
+    // .sort({ date: -1 })
+    .then((dbWorkout) => {
+      console.log("data", dbWorkout);
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/workouts/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  Workout.findOneAndUpdate({ _id: id }, { $push: { exercises: body } })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/workouts", (req, res) => {
+  Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(7)
+    .then((dbWorkout) => {
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
